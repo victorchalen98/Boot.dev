065041eda10e230dc2536e0061637aabf6f164e7
victorchalen98 <57729512+victorchalen98@users.noreply.github.com>
2025-09-16 10:45:05 -0500

    Testing L7

---
+++ 1.LearnToCodeInPython/5.TestingAndDebugging/l7_debugging_practice.py
@@ -0,0 +1,6 @@
+def unlock_achievement (before_xp, ach_xp, ach_name):
+    after_xp = before_xp + ach_xp
+    alert = "Achievement Unlocked: " + ach_name
+    return after_xp, alert
+
+unlock_achievement(100, 10, "Logro Increible")
