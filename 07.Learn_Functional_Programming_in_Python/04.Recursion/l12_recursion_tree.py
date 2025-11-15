6096d1530a340cfad2490bc4ec6a7c0f4f8f42d6
victorchalen98 <57729512+victorchalen98@users.noreply.github.com>
2025-11-14 22:01:03 -0500

    CH4 - L12:Recursion on a Tree

---
+++ 07.Learn_Functional_Programming_in_Python/04.Recursion/l12_recursion_tree.py
@@ -0,0 +1,11 @@
+def list_files(parent_directory, current_filepath=""):
+    file_paths = []
+    for key, value in parent_directory.items():
+        new_path = f"{current_filepath}/{key}"
+
+        if value == None:
+            file_paths.append(new_path)
+        else:
+            recursive_call = list_files(value, new_path)
+            file_paths.extend(recursive_call)
+    return file_paths
