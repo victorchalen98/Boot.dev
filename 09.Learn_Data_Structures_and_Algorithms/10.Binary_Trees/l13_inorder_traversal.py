763fae539f49e381302bede07382deceddff5e68
victorchalen98 <57729512+victorchalen98@users.noreply.github.com>
2026-01-02 11:51:04 -0500

    CH10 - L13:Inorder Traversal

---
+++ 09.Learn_Data_Structures_and_Algorithms/10.Binary_Trees/l13_inorder_traversal.py
@@ -0,0 +1,39 @@
+class BSTNode:
+    def inorder(self, visited):
+        if self.left:
+            self.left.inorder(visited)
+
+        visited.append(self.val)
+
+        if self.right:
+            self.right.inorder(visited)
+
+        return visited 
+
+    # don't touch below this line
+
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+
+        if self.val == val:
+            return
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)
+
