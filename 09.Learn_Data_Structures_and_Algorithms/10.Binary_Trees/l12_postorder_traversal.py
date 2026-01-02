34b7cf6c57e29dec0d25c4d71bfd0d5ad4973c34
victorchalen98 <57729512+victorchalen98@users.noreply.github.com>
2026-01-02 11:36:05 -0500

    CH10 - L12:Postorder Traversal

---
+++ 09.Learn_Data_Structures_and_Algorithms/10.Binary_Trees/l12_postorder_traversal.py
@@ -0,0 +1,38 @@
+class BSTNode:
+    def postorder(self, visited):
+        if self.left:
+            self.left.postorder(visited)
+        if self.right:
+            self.right.postorder(visited)
+
+        visited.append(self.val)
+
+        return visited
+
+    # don't touch below this line
+
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+
+        if self.val == val:
+            return
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)
+
---
+++ 09.Learn_Data_Structures_and_Algorithms/10.Binary_Trees/l12_test.py
@@ -0,0 +1,101 @@
+from l12_postorder_traversal import *
+from user import *
+import random
+
+run_cases = [
+    (
+        4,
+        [User(0), User(8), User(11), User(7)],
+    ),
+    (
+        6,
+        [User(0), User(9), User(5), User(17), User(16), User(10)],
+    ),
+]
+
+submit_cases = run_cases + [
+    (
+        12,
+        [
+            User(11),
+            User(10),
+            User(18),
+            User(17),
+            User(19),
+            User(2),
+            User(23),
+            User(27),
+            User(33),
+            User(30),
+            User(22),
+            User(34),
+        ],
+    ),
+]
+
+
+def test(num_characters, expected):
+    characters = get_users(
+        num_characters
+    )  # Ensure this reflects your project structure
+    bst = BSTNode()
+    for character in characters:
+        bst.insert(character)
+    print("=====================================")
+    print("Tree:")
+    print("-------------------------------------")
+    print(print_tree(bst))
+    print("-------------------------------------\n")
+    print(f"Expected: {expected}")
+    try:
+        actual = bst.postorder([])
+        print(f"Actual:   {actual}")
+        if expected == actual:
+            print("Pass \n")
+            return True
+        print("Fail \n")
+        return False
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    skipped = len(submit_cases) - len(test_cases)
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    if skipped > 0:
+        print(f"{passed} passed, {failed} failed, {skipped} skipped")
+    else:
+        print(f"{passed} passed, {failed} failed")
+
+
+def print_tree(bst_node):
+    lines = []
+    format_tree_string(bst_node, lines)
+    return "\n".join(lines)
+
+
+def format_tree_string(bst_node, lines, level=0):
+    if bst_node is not None:
+        format_tree_string(bst_node.right, lines, level + 1)
+        lines.append(" " * 4 * level + "> " + str(bst_node.val))
+        format_tree_string(bst_node.left, lines, level + 1)
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
+
