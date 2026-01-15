f5822525cc120195738b3c4ab79554e28416af7c
victorchalen98 <57729512+victorchalen98@users.noreply.github.com>
2026-01-15 09:52:15 -0500

    CH16 - L3:Traveling Salesman Problem

---
+++ 09.Learn_Data_Structures_and_Algorithms/16.P_vs_NP/l03_traveling_salesman_problem.py
@@ -0,0 +1,43 @@
+def tsp(cities, paths, dist):
+    all_routes = permutations(cities)
+
+    for route in all_routes:          # loop externo: cada permutación
+        total = 0
+
+        for i in range(len(route) - 1):   # loop interno: sumar tramos consecutivos
+            from_city = route[i]
+            to_city = route[i + 1]
+            total += paths[from_city][to_city]
+
+            # pequeña optimización: si ya se pasó, no sigas
+            if total >= dist:
+                break
+
+        if total < dist:
+            return True
+
+    return False
+
+
+# don't touch below this line
+
+
+def permutations(arr):
+    res = []
+    res = helper(res, arr, len(arr))
+    return res
+
+
+def helper(res, arr, n):
+    if n == 1:
+        tmp = arr.copy()
+        res.append(tmp)
+    else:
+        for i in range(n):
+            res = helper(res, arr, n - 1)
+            if n % 2 == 1:
+                arr[n - 1], arr[i] = arr[i], arr[n - 1]
+            else:
+                arr[0], arr[n - 1] = arr[n - 1], arr[0]
+    return res
+
